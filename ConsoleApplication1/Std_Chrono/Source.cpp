5847b84f93273b9c8fa40376efb67f602657f133
Liang Peng <liapeng@microsoft.com>
2017-10-20 10:23:30 -0700

    test chrono

---
+++ ConsoleApplication1/Std_Chrono/Source.cpp
@@ -0,0 +1,17 @@
+
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+int main()
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << std::endl;
+    
+    return 0;
+}
